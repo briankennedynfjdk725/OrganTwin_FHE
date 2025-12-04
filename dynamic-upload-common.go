f742ce32ced7db8408c4d5d2f18260445b923f55
briankennedynfjdk725 <StassinosMilbrandt5430@outlook.com>
2025-12-04 23:53:16 +0800

    add subscriptions-demo.rs, ZamaConfig.json, dynamic-upload-common.go

---
+++ dynamic-upload-common.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\dynamic-upload-common.go
+// Generated at: 2025-10-16T16:01:01.280Z
---
+++ subscriptions-demo.rs
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\subscriptions-demo.rs
+// Generated at: 2025-10-16T16:01:01.282Z
